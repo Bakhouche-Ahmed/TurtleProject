0ab0148957a90b6b29540440ac2749d4127ab2fe
Ahmed Bakhouche <contactahmedadib@gmail.com>
2018-04-09 16:32:45 +0200

    Ajout d'un compteur

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
 <?php
 echo "<h1>The Turtle Project</h1>";
+
+$message = "<ul>";
+
+for ($i = -17827; $i <= 14568; $i += 7){
+    if($i % 24 == 0){
+        $message .= "<li>";
+        $message .= $i;
+        $message .= "</li>";
+    }
+}
+
+$message .= "</ul>";
+
+echo $message;
